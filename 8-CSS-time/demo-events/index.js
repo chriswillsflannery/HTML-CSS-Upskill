9db4c135efa6b2f40c373cd963e8b06f4aec0d14
Chris Flannery <chriswillsflannery@gmail.com>
2020-11-14 17:28:59 -0500

    add events demo

---
+++ 8-CSS-time/demo-events/index.js
@@ -0,0 +1,29 @@
+console.log('loaded js');
+
+const hoverBlock = document.querySelector('.hoverable__div1');
+
+// hoverBlock.addEventListener('transitionrun', () => {
+//   console.log('%ctransition was run!', 'color: green');
+// });
+
+// hoverBlock.addEventListener('transitionend', () => {
+//   console.log('%ctransition ended!', 'color: orange');
+// });
+
+// hoverBlock.addEventListener('transitioncancel', () => {
+//   console.log('%ctransition was cancelled', 'color: red');
+// });
+
+const animateBlock = document.querySelector('.hoverable__div2');
+
+// animateBlock.addEventListener('animationstart', () => {
+//   console.log('%canimation started', 'color: green');
+// });
+
+// animateBlock.addEventListener('animationiteration', () => {
+//   console.log('%canimation iteration', 'color: yellow');
+// });
+
+// animateBlock.addEventListener('animationend', () => {
+//   console.log('%canimation end', 'color: lightblue');
+// });
